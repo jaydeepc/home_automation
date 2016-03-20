7d5b8e0e8f3695392063ff8234a013eb4497ce3b
System Administrator <root@INjaydeepc.local>
2016-03-20 16:26:03 +0530

    Added Jquery

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ $('#switch_it').on('change', function(){
     }
 });
 </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 </head>
 <title>Home Automation Dashboard</title>
 <body>
