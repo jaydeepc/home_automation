24e141b874b6fa7b7f96a73c32f6bd7609be0c16
System Administrator <root@INjaydeepc.local>
2016-03-20 17:12:57 +0530

    Added Jquery again and again

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 $ctl = $_POST['ctl'];
+echo "<script type='text/javascript'>alert('$ctl');</script>";
 $command = "sudo python ../relay.py $ctl";
 $result = shell_exec($command);
 echo($result);
@@ -19,15 +20,6 @@ echo($result);
 <link rel="shortcut icon" href="../favicon.ico"> 
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300' rel='stylesheet' type='text/css' />
-<script>
-$('#switch_it').on('change', function(){
-    if ($(this).is(':checked')) {
-        document.getElementById("myForm").submit();
-    } else {
-//        $('form').attr('action', 'invoiceCreate.php');
-    }
-});
-</script>
 </head>
 <title>Home Automation Dashboard</title>
 <body>
@@ -36,22 +28,30 @@ $('#switch_it').on('change', function(){
         <h1>Control Lights</h1>
 </header>
 
-    <form action="" method="POST" name="form" id="myForm">
+<form action="" method="POST" name="form" id="myForm">
 <section class="main">
         <div class="switch demo2">
-            <input type="checkbox" id="switch_it">
+            <input type="checkbox" id="switch_it" checked>
                 <label></label>
         </div>
-        <input type="hidden" name="ctl" value="1">
+    <input type="hidden" name="ctl" value="" id="ctl_value">
+    <script type="text/javascript">  
+        $(function(){
+         $('#switch_it').on('change',function(){
+         if ($('#switch_it').is(':checked')){
+             $('#ctl_value').val(0);
+             $('#myForm').submit();
+         }
+         else{
+             $('#ctl_value').val(1);
+             $('#myForm').submit();
+         }
+         
+            });
+        });
+    </script>        
 </section>
 </form></p>
-
-<form action="" method="POST" name="form">
-<p class="lol">
-<input type="submit" id="off" value="OFF">
-<input type="hidden" name="ctl" value="0">
-</form>
-</p>
 
 </div>
 </body></html>
